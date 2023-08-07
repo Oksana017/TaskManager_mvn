d88f4a762124a707cc05e10357b99f6a04d03511
Oksana <oksana017m@mail.com>
2023-08-07 22:30:43 +0300

    change src

---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,79 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+
+    public void simpleTaskMatchesTheRequest() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("Позвонить");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+
+    public void epicMatchesTheRequest() {
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Хлеб");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+
+    public void meetingMatchesTheRequest() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches("Приложение");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+
+    public void simpleTaskDoesNotMatchesTheRequest() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("Написать");
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+
+    public void epicDoesNotMatchesTheRequest() {
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("молоко");
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+
+    public void meetingDoesNotMatchesTheRequest() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches("Нетобанк");
+
+        Assertions.assertFalse(actual);
+    }
+}
+
